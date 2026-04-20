366567fc418cb7b29d742757597504af65c4f83d
Ruslan Khasanshin <r.m.khasanshin@gmail.com>
2026-04-21 03:59:31 +0600

    chore: fixes

--- scripts/set_dependabot_secret.sh
+++ scripts/set_dependabot_secret.sh
@@ -6,26 +6,26 @@ SECRET_NAME="GHCR_PAT"
 SECRET_VALUE=""
 
 REPOS=(
-	hu553in/spa-crawler
-	hu553in/gdrive-assistant-bot
-	hu553in/keenetic-vpn-switcher
-	hu553in/grooming-studio-chat-helper
-	hu553in/yandex-alice-openai
-	hu553in/grooming-studio-api
-	hu553in/dockguard
-	hu553in/telekilogram
-	hu553in/tempstream
-	hu553in/invites-keycloak
-	hu553in/grooming-studio
-	hu553in/wotd-bot
-	hu553in/cash-bot
-	hu553in/tizen-tool
-	hu553in/single-deployment
+  hu553in/spa-crawler
+  hu553in/gdrive-assistant-bot
+  hu553in/keenetic-vpn-switcher
+  hu553in/grooming-studio-chat-helper
+  hu553in/yandex-alice-openai
+  hu553in/grooming-studio-api
+  hu553in/dockguard
+  hu553in/telekilogram
+  hu553in/tempstream
+  hu553in/invites-keycloak
+  hu553in/grooming-studio
+  hu553in/wotd-bot
+  hu553in/cash-bot
+  hu553in/tizen-tool
+  hu553in/single-deployment
 )
 
 for repo in "${REPOS[@]}"; do
-	gh secret set "$SECRET_NAME" \
-		--app dependabot \
-		--repo "$repo" \
-		--body "$SECRET_VALUE"
+  gh secret set "$SECRET_NAME" \
+    --app dependabot \
+    --repo "$repo" \
+    --body "$SECRET_VALUE"
 done
